bceebb8d3339c88cc6354384016c3ece114fc96b
jakehoogstrate <jakehoogstrate5@gmail.com>
2025-04-25 09:35:23 -0400

    final

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-    t.window.width = 1280  -- Set width (Increase as needed)
-    t.window.height = 720  -- Set height (Increase as needed)
-    t.window.title = "Shadow Painter"  -- Custom window title
+    t.window.width = 1280  
+    t.window.height = 720  
+    t.window.title = "Shadow Painter"
 end
 
